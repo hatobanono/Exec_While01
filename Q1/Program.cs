164b0ce980c1f1c37b31e99eda81655d1ee39b5c
hatobanono <chris00iq@gmail.com>
2022-10-24 18:14:19 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int limit = 105;
+			int currentNumber = 0;
+			int sum = 0;
+
+			while (sum < limit)
+			{
+				currentNumber++;
+				sum += currentNumber;
+			}
+			Console.WriteLine(currentNumber-1);
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = 300;
+			int i = 1;
+			while (i <= number)
+			{
+				if (number % i == 0)
+				{
+					Console.WriteLine(i);
+				}
+				i++;
+			}
+		}
+	}
+}
